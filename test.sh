acfd5aa9c32e39195552bb8d25bd50e3ae3d540c
ts123 <hukurou@gmail.com>
2013-12-11 06:49:41 +0900

    fix script

--- test.sh
+++ test.sh
@@ -8,10 +8,10 @@ cmd uname -a
 cmd id
 cmd pwd
 cmd 'env | sort'
-cmd ls /bin
-cmd ls /sbin
-cmd ls /usr/bin
-cmd ls /usr/sbin
-cmd ls /usr/local/bin
-cmd ls /usr/local/sbin
+cmd 'echo `ls /bin`'
+cmd 'echo `ls /sbin`'
+cmd 'echo `ls /usr/bin`'
+cmd 'echo `ls /usr/sbin`'
+cmd 'echo `ls /usr/local/bin`'
+cmd 'echo `ls /usr/local/sbin`'
 
