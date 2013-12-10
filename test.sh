9bee08dfc283a82d9639c1e9c10a39988d30af67
ts123 <hukurou@gmail.com>
2013-12-11 06:34:19 +0900

    fix script

--- test.sh
+++ test.sh
@@ -5,11 +5,16 @@ id
 pwd
 env | sort
 set +x
+echo
 echo $ ls /bin
 echo `ls /bin`
+echo
 echo $ ls /sbin
 echo `ls /sbin`
+echo
 echo $ ls /usr/bin
 echo `ls /usr/bin`
+echo
 echo $ ls /usr/sbin
 echo `ls /usr/sbin`
+
